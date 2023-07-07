2f7bb0893fef3826c496d71d1ae9ea9353850cff
Jory Roberts <robertsjory13@gmail.com>
2023-07-07 13:50:05 -0500

    completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    first_value = 0
+    second_value = 1
+    temp_value = 0
+
+    for i in range(length):
+        if i in (0, 1):
+            fibonacci_list.append(i)
+        else:
+            fibonacci_list.append(first_value + second_value)
+            temp_value = second_value
+            second_value = second_value + first_value
+            first_value = temp_value
+
+    print(fibonacci_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
